2dc386ba6be841c548b60953e5909f4f8ba264de
taichi-master <k_wong@yahoo.com>
2018-04-10 18:59:03 -0400

    removed bootstrap link

--- server/app.js
+++ server/app.js
@@ -38,7 +38,6 @@ function createPage(pkg, style, initialState, appHtml) {
         <meta name="keywords" content="${pkg.keywords.join(', ')}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         ${style}
         <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};</script>
       </head>
